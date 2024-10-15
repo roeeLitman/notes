0518613c0ddb9704cd12accece92ee0a329940bb
roee ltmanovich <roeeli770@gmail.com>
2024-10-15 12:09:05 +0300

    bgin to worke on user service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,10 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './entities/user.interfce';
+import {Model} from 'mongoose'
+import * as bcrypt from 'bcrypt'
+
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
+  constructor(@InjectModel('User') private userModel: Model<User>){}
+
+  async create(createUserDto: CreateUserDto) {
+    try {
+      const hashPassword = await bcrypt.hash(createUserDto.password, 10)
+      const newUser = new this.userModel({...createUserDto, password: hashPassword})
+      return newUser.save()
+    } catch (err) {
+      throw new BadRequestException()
+    }
     return 'This action adds a new user';
   }
 
