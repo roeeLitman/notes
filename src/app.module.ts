101833ea3fd2499fcf50a710f34d6a363df16a1c
roee ltmanovich <roeeli770@gmail.com>
2024-10-15 12:07:41 +0300

    import coocki and dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { UserModule } from './user/user.module';
 import { NoteModule } from './note/note.module';
 import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import 'dotenv/config'
 
 @Module({
   imports: [UserModule, NoteModule, AuthModule,MongooseModule.forRoot(process.env.connect_string)],
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import cookieParser  from 'cookie-parser'
+import * as cookieParser  from 'cookie-parser'
 
 async function bootstrap() {
   const PORT = process.env.PORT || 1414
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser())
   await app.listen(PORT, () => {console.log( `http://l:${PORT}`);
   });
 }
